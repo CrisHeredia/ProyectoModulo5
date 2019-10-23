74242c1bbf4716b128c7c9b06b690bee11a45a71
CristinaHeredia <crisheredian@hotmail.com>
2019-10-23 11:13:42 -0500

    Puntos 4
    Implementación punto 4 y modificaciones en otros puntos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+$(function(){
+  $(".elemento").hide();
+  tiempo = 0;
+  min=2;
+  seg=0;
+  //intervalo = 0;
+  //eliminar = 0;
+
+  prenderTitulo();
+  $(".btn-reinicio").on("click", function(){
+    score=0;
+  	mov=0;
+    $(".btn-reinicio").text("Reiniciar");
+    $(".elemento").show();
+    $(".time").show();
+    $(".panel-score").css("width","25%");
+    $("#score-text").html("0");
+  	$("#movimientos-text").html("0");
+    clearInterval(tiempo);
+    min=2;
+    seg=0;
+    //clearInterval(intervalo);
+    //clearInterval(eliminar);
+    ordenarBloques();
+    //verificarCoincidencias();
+    //borrarRepetidos();
+    tiempo=setInterval(function(){
+  		timer()
+  	},1000);
+    //intervalo=setInterval(function(){
+  		//ordenarBloques()
+  	//},600);
+    //rellenarBloques();
+  })
+});
+
 function prenderTitulo(){
   $(".main-titulo").animate({color:"#606060"}, 500, function(){
       apagarTitulo();
@@ -13,6 +49,7 @@ function apagarTitulo(){
 }
 
 function ordenarBloques(){
+  $(".elemento").draggable({disabled:true});
   var arreglo = $('.elemento').sort(function(a, b){return 0.5 - Math.random()});
   for (var i = 0; i < 50; i++) {
     var elemento1 = $('.elemento')[i];
@@ -21,87 +58,134 @@ function ordenarBloques(){
     var nombre2 = $(elemento2).attr('src');
     $(elemento1).attr("src",nombre2)
   }
+  //if (i == 50){
+    //clearInterval(intervalo);
+  	//eliminar=setInterval(function(){
+  borrarRepetidos();
+  	//,150);
+  //}
 }
 
-function verificarCoincidencias(){
-  // Barrida del arreglo de elementos para verificar filas y columnas que tienen coincidencias
-  arraycoincide = [];
-  var arreglo = $('.elemento');
+function verificarRepetidosHori(){
+  arreglo = $('.elemento');
+  var horizontal = 0;
   for (var i = 0; i < 50; i++) {
     var elemento = $('.elemento')[i];
-    var posarriba = $('.elemento')[i-1];
-    var posabajo = $('.elemento')[i+1];
     var posizquierda = $('.elemento')[i-7];
     var posderecha = $('.elemento')[i+7];
-    if ($(elemento).attr('src') == $(posarriba).attr('src') && $(elemento).attr('src') == $(posabajo).attr('src')){
-      if (i != 6 && i != 7 && i != 13 && i != 14 && i != 20 && i != 21 && i != 27 && i != 28 && i != 34 && i != 35 && i != 41 && i != 42){
-        $(arreglo[i-1]).attr("class","elemento activo");
-        $(arreglo[i]).attr("class","elemento activo");
-        $(arreglo[i+1]).attr("class","elemento activo");
-        //arraycoincide.push([i-1]);
-        //arraycoincide.push([i]);
-        //arraycoincide.push([i+1]);
-      }
-    }
     if ($(elemento).attr('src') == $(posizquierda).attr('src') && $(elemento).attr('src') == $(posderecha).attr('src')){
       if (i != 42 && i != 1 && i != 43 && i != 2 && i != 44 && i != 3 && i != 45 && i != 4 && i != 46 && i != 5 && i != 47 && i != 6){
         $(arreglo[i-7]).attr("class","elemento activo");
         $(arreglo[i]).attr("class","elemento activo");
         $(arreglo[i+7]).attr("class","elemento activo");
-        //arraycoincide.push([i-7]);
-        //arraycoincide.push([i]);
-        //arraycoincide.push([i+7]);
+        horizontal = 1;
       }
     }
   }
-  console.log(arreglo);
+  return horizontal;
+}
+
+function verificarRepetidosVerti(){
+  arreglo = $('.elemento');
+  var vertical = 0;
+  for (var i = 0; i < 50; i++) {
+    var elemento = $('.elemento')[i];
+    var posarriba = $('.elemento')[i-1];
+    var posabajo = $('.elemento')[i+1];
+    if ($(elemento).attr('src') == $(posarriba).attr('src') && $(elemento).attr('src') == $(posabajo).attr('src')){
+      if (i != 6 && i != 7 && i != 13 && i != 14 && i != 20 && i != 21 && i != 27 && i != 28 && i != 34 && i != 35 && i != 41 && i != 42){
+        $(arreglo[i-1]).attr("class","elemento activo");
+        $(arreglo[i]).attr("class","elemento activo");
+        $(arreglo[i+1]).attr("class","elemento activo");
+        vertical = 1;
+      }
+    }
+  }
+  return vertical;
 }
 
 function borrarRepetidos(){
-  //for (var i = 0; i < arraycoincide.length; i++){
-    //var indice = arraycoincide[i];
-
-    //$(arreglo[indice]).attr("alt","pasivo");
-    //$(arreglo[indice]).hide("pulsate", 1000, function(){
+  repetidoshori = verificarRepetidosHori();
+  repetidosverti = verificarRepetidosVerti();
+  total = $('.elemento').length;
+  //alert (repetidoshori,"  ",repetidosverti,"  ",total);
+  if(repetidoshori == 1 || repetidosverti == 1){
+    //alert ("si coincidencias");
+    //$(".elemento").draggable({disabled:true});
+    nroborrar = $('.activo').length;
     $(".activo").hide("pulsate", 1000, function(){
-      //arreglo.splice(indice, 1);
-      $(".activo").remove("img");
-      //delete $(arreglo)[indice];
-      //$(arreglo)[indice].detach("img");
-      //$(arreglo)[indice] = "";
-    })
-  //}
 
+      $(".activo").remove("img");
+      score = score + (nroborrar * 10);
+      $("#score-text").html(score);
+      //buscarNuevosDulces=0;
+  		//nuevosDulces=setInterval(function(){
+  		rellenarBloques();
+  		//},600);
+      borrarRepetidos();
+      //score = score + scoretmp * 10;
+    });
+
+  }
+  //if(repetidoshori == 0 && repetidosverti == 0 && total == 49){
+    //alert ("no coincidencias, panel lleno");
+
+  //}
+  //if(repetidoshori == 0 && repetidosverti == 0 && total != 49){
+    //alert ("no coincidencias, panel no lleno");
+		//clearInterval(eliminar);
+		//buscarNuevosDulces=0;
+		//rellenarBloques=setInterval(function(){
+		//rellenarBloques();
+		//},600);
+    //borrarRepetidos();
+  //}
 }
 
 function rellenarBloques(){
-  alert("rellenar bloques");
-  //console.log (arreglo);
-  columna1 = $('.col-1').find(".elemento");
-  //vacio = $(columna1).attr("alt");
-  //console.log (columna1);
-  //console.log (vacio);
-  var j = 6;
-  do {
-    var elemento1 = $(columna1)[j];
-    var nombre1 = $(elemento1).attr("alt");
-    if (nombre1 == "pasivo"){
-      $(columna1[j]).replaceWith(columna1[j-1]);
+  for (var j = 1; j < 8; j++) {
+    columna = $(".col-"+j).find(".elemento");
+    nrollenar = 7 - columna.length;
+    if (nrollenar > 0){
+      for (var k = 1; k < nrollenar+1; k++){
+        numero=Math.floor(Math.random()*4)+1;
+        imagen="image/"+numero+".png";
+
+        $(".col-"+j).prepend("<img src="+imagen+" class='elemento'/>");//.css("display","none");
+        //$(".col-"+j[nrollenar-k]).hide();
+        //$(".col-"+j[0]).slideDown(4000);
+
+      }
     }
-    j -= 1;
-  } while (j < 7 && j>=0);
-  console.log (columna1);
+  }
+  arreglo = $(".elemento");
+  console.log (arreglo)
+  //clearInterval(nuevosDulces);
+  //eliminar=setInterval(function(){
+    //eliminarhorver()
+  //},150);
 }
 
-$(function(){
-  $(".elemento").hide();
-  $(".btn-reinicio").on("click", function(){
-    $(".btn-reinicio").text("Reiniciar");
-    $(".elemento").show();
-    prenderTitulo();
-    ordenarBloques();
-    verificarCoincidencias();
-    borrarRepetidos();
-    //rellenarBloques();
-  })
-});
+function timer(){
+	if(seg!=0){
+		seg=seg-1;}
+	if(seg==0){
+		if(min==0){
+			//clearInterval(eliminar);
+			//clearInterval(nuevosDulces);
+			//clearInterval(intervalo);
+			clearInterval(tiempo);
+			$(".panel-tablero").hide("drop","slow",abrirPantalla);
+			$(".time").hide();}
+		seg=59;
+		min=min-1;}
+  if(seg<10 && seg>=0){
+    seg = "0"+seg;
+  }
+	$("#timer").html("0"+min+":"+seg);
+};
+
+function abrirPantalla(){
+	$( ".panel-score" ).animate({width:'100%'},3000);
+	$(".termino").css({"display":"block","text-align":"center"});
+};
